824ccd09f8eb07ed772520776f7b892e6ec9b19f
DariaRodovnichenko <DariaRodovnichenko@users.noreply.github.com>
2023-07-11 12:31:40 +0200

    first-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function start() {
+  btnStart.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stop() {
+  btnStart.disabled = false;
+  clearInterval(intervalId);
+}
+
+btnStart.addEventListener('click', start);
+btnStop.addEventListener('click', stop);
